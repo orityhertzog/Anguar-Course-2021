52911401f7f79c90f8b03e7ecc83551ba6988d6e
Orit Hertzog <orityhertzog@gmail.com>
2021-05-25 17:00:01 +0300

    logic improved

--- Day14/ex7-calc/src/app/app.component.ts
+++ Day14/ex7-calc/src/app/app.component.ts
@@ -10,46 +10,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ex7-calc';
-  //date
+  //data
   num1 :number = 0;
   num2 :number = 0;
-  results :string[] = [];
+  
+  sum :number | null = null;
+  product :number | null = null;
+  subtract :number | null = null;
 
-  //method
-
-  setNum1(val1 :string){
-    this.num1 = Number(val1);
+  get hasResults() :boolean {
+    return (this.sum == null) && (this.product == null) && (this.subtract == null);
   }
 
-  setNum2(val2 :string){
-    this.num2 = Number(val2);
+  //methods
+  reset(){
+    this.sum = null;
+    this.product = null;
+    this.subtract = null;
+}
+
+  setNum1(value :string){
+     this.num1 = Number(value);
+     this.reset();
   }
 
-  print(a :number, b: number, res: number, symbol :string) :string{
-    return `${a} ${symbol} ${b} = ${res}`;
-  }
+  setNum2(value :string){
+    this.num2 = Number(value);
+    this.reset();
+ }
 
-  add(a :number, b :number) :number{
-    return  a + b;
-    
-  }
-
-  multiply(a :number, b :number) :number{
-    return  a * b;
-   
-  }
-
-  subtract(a :number, b :number) :number{
-    return a - b;
-    
-  }
-
-  log() :void{
-    let addRes :number = this.add(this.num1, this.num2);
-    let multiplyRes :number = this.multiply(this.num1, this.num2);
-    let subtractRes :number = this.subtract(this.num1, this.num2);
-    this.results[0] = this.print(this.num1,this.num2,addRes,"+");
-    this.results[1] = this.print(this.num1,this.num2,multiplyRes,"*")
-    this.results[2] = this.print(this.num1,this.num2,subtractRes, "-")
-  }
+ calc(){
+   this.sum = this.num1 + this.num2;
+   this.product = this.num1 * this.num2; 
+   this.subtract = this.num1 - this.num2;
+ }
 }
